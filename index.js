a47a1747faf2b2d47be963028ead7ea3d182b4ac
rbarretto <richard.a.barretto@gmail.com>
2017-11-14 01:46:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+const  currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
