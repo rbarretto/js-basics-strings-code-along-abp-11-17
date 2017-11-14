d2d138deb85f128690a0b774de8d793a86eff7ae
rbarretto <richard.a.barretto@gmail.com>
2017-11-14 01:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 //
 const shortGreeting = 'Welcome, ${currentUser}.charAt(0)';
-
