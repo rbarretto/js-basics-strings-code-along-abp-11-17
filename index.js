a18c031de9c5089a2992e129836fb3e2cd89d32c
rbarretto <richard.a.barretto@gmail.com>
2017-11-14 01:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+const  currentUser = 'Grace Hopper';
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
